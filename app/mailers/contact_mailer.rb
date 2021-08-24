34c77b3e9ca445adb2041d0243330aec8a65f38f
AnushaMutyala <anusha@entersoftsecurity.com>
2021-08-24 16:15:44 +0530

    added smtp settings

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,10 @@
 class ContactMailer < ApplicationMailer
   default from: 'anusha242316@gmail.com'
 
-  CONTACT_EMAIL = "anusha242316@gmail.com"
+  CONTACT_EMAIL = "swamulumuthyala@gmail.com"
 
   def submission(message)
     @message = message
-    mail(to: CONTACT_EMAIL, subject: 'New contact page submission')
+    mail to: CONTACT_EMAIL, subject:"New contact page submission"
   end
 end
